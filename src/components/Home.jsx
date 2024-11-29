4aec9f280b84d484bc5da7c1c8eba4e93dbce966
SkyBlueHeat <br.aydin@hotmail.com>
2024-11-29 16:17:07 +0300

    tailwind

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import home2 from './../../Assets/Iteration-1-assets/logo.svg'
 import banner from './../../Assets/Iteration-1-assets/home-banner.png'
 function Home() {
   return (
-    <div className='min-h-screen flex flex-row justify-between items-center lg:px-32 px-5 bg-[url(./../../Assets/Iteration-1-assets/home-banner.png)] bg-cover bg-no-repeat'>
+    <div>
         <div>
             <div>
               <h1>KOD ACIKTIRIR< br ></br> PİZZA, DOYURUR</h1>
