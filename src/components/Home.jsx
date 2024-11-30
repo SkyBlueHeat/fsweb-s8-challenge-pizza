d8f754af9ebc0437f09c7ae6f54865956b8a162c
SkyBlueHeat <br.aydin@hotmail.com>
2024-11-30 05:51:10 +0300

    Fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import './Home.css'
 import home2 from './../../Assets/Iteration-1-assets/logo.svg'
 import banner from './../../Assets/Iteration-1-assets/home-banner.png'
 function Home() {
   return (
-    <div>
-        <div>
-            <div>
-              <h1>KOD ACIKTIRIR< br ></br> PİZZA, DOYURUR</h1>
-            </div> 
-            <div>
-           <button className=''>ACIKTIM</button>
-            </div>
-            
-        </div>
-    </div>
+   <div >
+    <header >
+      <div className='img-container'>
+      <div>
+      <img  src="Assets\Iteration-1-assets\logo.svg" alt="" className='form-img' />
+      <p className='anasayfa-text'>AnaSayfa - <span className='siparis'>Sipariş Oluştur</span></p>
+      </div>        
+      </div>      
+    </header>
+
+    <section className='hero-section'>
+          <div>
+              <h3>Position Absolute Acı Pizza</h3>
+          </div>         
+    </section>
+   </div>
+
+   
+   
   ) 
 }
 
